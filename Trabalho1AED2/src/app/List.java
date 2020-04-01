554def154ebc900532f4689d3bb88dc6c7f4c13d
Daniel Barreto <danielbarreto559@gmail.com>
2020-04-01 12:10:11 -0300

    Create List

---
+++ Trabalho1AED2/src/app/List.java
@@ -0,0 +1,17 @@
+package app;
+
+public class List {
+    private Node first;
+    private Node last;
+    private int qtd;
+
+    public List() {
+        this.first = null;
+        this.last = null;
+        this.qtd = 0;
+    }
+
+    
+
+    
+}
