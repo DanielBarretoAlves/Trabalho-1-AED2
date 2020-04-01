4737246df0cd745d4bb1d6b646af61ea9c65eefe
Daniel Barreto <danielbarreto559@gmail.com>
2020-04-01 14:22:37 -0300

    Insert first + is empity

--- Trabalho1AED2/src/app/List.java
+++ Trabalho1AED2/src/app/List.java
@@ -11,6 +11,44 @@ public class List {
         this.qtd = 0;
     }
 
+    public Node getFirst() {
+        return first;
+    }
+
+    public void setFirst(Node first) {
+        this.first = first;
+    }
+
+    public Node getLast() {
+        return last;
+    }
+
+    public void setLast(Node last) {
+        this.last = last;
+    }
+
+    public int getQtd() {
+        return qtd;
+    }
+
+    public void setQtd(int qtd) {
+        this.qtd = qtd;
+    }
+
+    public void insertFirst(TBlocos tb)//---------------------INSERT FIRST
+    {
+        Node newNode = new Node(tb);
+        if (isEmpity()) {
+            this.last = newNode;
+
+        }
+        newNode.setNext(this.first);
+        this.qtd++;
+
+    }
+
+    public boolean isEmpity(){return(this.first == null);}//-------------IS EMPITY
+
     
 
     
